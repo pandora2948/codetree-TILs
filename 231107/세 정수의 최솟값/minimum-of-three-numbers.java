da6013a9baecf1684b6067a06e609f4a65de0210
Baek Dongjae <kgdj030@gmail.com>
2023-11-07 23:57:49 +0900

    Title: 세 정수의 최솟값, Time: 176ms, Memory: 10MB, Status: Passed - CodeTree

--- 231107/세 정수의 최솟값/minimum-of-three-numbers.java
+++ 231107/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -9,8 +9,8 @@ public class Main {
         b = sc.nextInt();
         c = sc.nextInt();
 
-        if (a < b && a < c) System.out.println(a);
-        else if (b < a && b < c) System.out.println(b);
+        if (a <= b && a <= c) System.out.println(a);
+        else if (b <= a && b <= c) System.out.println(b);
         else System.out.println(c);
     }
 }
