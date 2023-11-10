12f3d42ee8dae84af33394330d2d11e53fdf796c
Baek Dongjae <kgdj030@gmail.com>
2023-11-10 19:06:34 +0900

    Title: 청소당번 정하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231110/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int classroom = 0, floor = 0, bathroom = 0;
+
+        for (int i = 1; i < n; i += 1) {
+            if (i % 12 == 0) {
+                bathroom += 1;
+            }
+            else if (i % 3 == 0) {
+                floor += 1;
+            }
+            else if (i % 2 == 0) {
+                classroom += 1;
+            }
+        }
+        System.out.printf("%d %d %d", classroom, floor, bathroom);
+    }
+}
