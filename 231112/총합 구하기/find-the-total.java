f62855d1c52d510454139b04859f9c3991202def
Baek Dongjae <kgdj030@gmail.com>
2023-11-12 23:56:20 +0900

    Title: 총합 구하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231112/총합 구하기/find-the-total.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a, b, res = 0;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        for (int i = a; i <= b; i += 1) {
+            if (i % 6 == 0 && i % 8 != 0) res += i;
+        }
+        System.out.print(res);
+    }
+}
