1343fe316c2e99a77a04d6fac78518c695463d17
Baek Dongjae <kgdj030@gmail.com>
2023-11-13 23:22:34 +0900

    Title: 1부터 곱셈하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231113/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int i, n = sc.nextInt(), res = 1;
+
+        for (i = 1; i <= 10; i += 1) {
+            res *= i;
+            if (res > i) break;
+        }
+        System.out.print(i);
+    }
+}
