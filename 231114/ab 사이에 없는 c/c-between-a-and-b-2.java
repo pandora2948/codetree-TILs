99d8679bbfee5500d2d4f8cbcbfa4d869b375771
Baek Dongjae <kgdj030@gmail.com>
2023-11-14 18:12:54 +0900

    Title: ab 사이에 없는 c, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231114/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        boolean isNotExist = true;
+
+        for (int i = a; i <= b; i += 1) {
+            if (i % c == 0) {
+                isNotExist = false;
+                break;
+            }
+        }
+        System.out.print(isNotExist ? "YES" : "NO");
+    }
+}
