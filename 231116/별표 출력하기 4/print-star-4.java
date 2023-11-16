f7c4535d3fe641c45a0ae75b5eaa11dee5ec9fa3
Baek Dongjae <kgdj030@gmail.com>
2023-11-16 13:55:50 +0900

    Title: 별표 출력하기 4, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231116/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = n; i > 0; i -= 1) {
+            for (int j = 0; j < i; j += 1) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 2; i <= n; i += 1) {
+            for (int j = 0; j < i; j += 1) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
