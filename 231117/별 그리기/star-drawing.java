7af135cfc2af62f9bcdc9425a355fed736b2f525
Baek Dongjae <kgdj030@gmail.com>
2023-11-17 15:22:49 +0900

    Title: 별 그리기, Time: 378ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 231117/별 그리기/star-drawing.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n - i - 1; j += 1) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (i + 1) * 2 - 1; k += 1) {
+                System.out.print("*");
+            }
+            for (int l = 0; l < n - i - 1; l += 1) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n ; i += 1) {
+            for (int j = 0; j < i; j += 1) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (n - i) * 2 - 1; k += 1) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
