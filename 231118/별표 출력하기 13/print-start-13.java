2a39c69ceee0157b6a9f0be7d5bd8bca0e31e96f
Baek Dongjae <kgdj030@gmail.com>
2023-11-18 19:39:57 +0900

    Title: 별표 출력하기 13, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231118/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n * 2; i += 1) {
+            if (i % 2 != 0) {
+                for (int j = 0; j < i / 2 + 1; j += 1) {
+                    System.out.print("* ");
+                }
+            }
+            else {
+                for (int j = 0; j < n - (i / 2); j += 1) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
