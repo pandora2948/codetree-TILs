ae95bdf5e66f9e2622e81acfd8a135caaa757c4e
Baek Dongjae <kgdj030@gmail.com>
2023-11-19 02:39:56 +0900

    Title: 별표 출력하기 12, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231119/별표 출력하기 12/print-star-12.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 != 0) {
+            for (int i = 0; i < n - 1; i += 1) {
+                for (int j = 0; j < n; j += 1) {
+                    if (i == 0 || (j % 2 == 1 && i <= j)) {
+                        System.out.print("* ");
+                    }
+                    else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        else {
+            for (int i = 0; i < n; i += 1) {
+                for (int j = 0; j < n; j += 1) {
+                    if (i == 0 || (j % 2 == 1 && i <= j)) {
+                        System.out.print("* ");
+                    }
+                    else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
