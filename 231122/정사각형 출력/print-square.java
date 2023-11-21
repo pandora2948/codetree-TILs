22efa2c05b40f047c237f179325f7a7c69a2729a
Baek Dongjae <kgdj030@gmail.com>
2023-11-22 05:34:18 +0900

    Title: 정사각형 출력, Time: 460ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 231122/정사각형 출력/print-square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), cnt = 1;
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                System.out.printf("%d ", cnt);
+                cnt += 1;
+            }
+            System.out.println();
+        }
+    }
+}
