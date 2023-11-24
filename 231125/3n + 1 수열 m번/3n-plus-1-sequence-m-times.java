e69992af603fae894cc113934c0073f625f90a4a
Baek Dongjae <kgdj030@gmail.com>
2023-11-25 00:11:02 +0900

    Title: 3n + 1 수열 m번, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231125/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i += 1) {
+            int n = sc.nextInt(), cnt = 0;
+            while (n != 1) {
+                n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+                cnt += 1;
+            }
+            System.out.println(cnt);
+        }
+    }
+}
