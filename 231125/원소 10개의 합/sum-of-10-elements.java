32b9385d5dab963490db880c035c09ba7ae79e24
Baek Dongjae <kgdj030@gmail.com>
2023-11-25 11:10:56 +0900

    Title: 원소 10개의 합, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231125/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0;
+
+        for (int i = 0; i < 10; i += 1) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        System.out.print(sum);
+    }
+}
