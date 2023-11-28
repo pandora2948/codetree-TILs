2cfb801c8304533919274fc526a198591c8dc451
Baek Dongjae <kgdj030@gmail.com>
2023-11-28 19:36:17 +0900

    Title: 특정 위치의 숫자, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231128/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0;
+
+        for (int i = 0; i < 10; i += 1) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i += 1) {
+            if (i == 2 || i == 4 || i == 9) {
+                sum += arr[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
