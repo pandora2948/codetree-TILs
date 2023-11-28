7b315239d2685df61f6c075cfd6ad3f6fb5b97c1
Baek Dongjae <kgdj030@gmail.com>
2023-11-28 22:35:22 +0900

    Title: 100 도달하기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/100 도달하기/reach-100.java
+++ 231128/100 도달하기/reach-100.java
@@ -5,11 +5,11 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), index = 0;
-        int[] arr = new int[12];
+        int[] arr = new int[13];
         arr[0] = 1;
         arr[1] = n;
 
-        for (int i = 2; i < 12; i += 1) {
+        for (int i = 2; i < 13; i += 1) {
             arr[i] = arr[i - 2] + arr[i - 1];
             if (arr[i - 1] >= 100) {
                 index = i;
