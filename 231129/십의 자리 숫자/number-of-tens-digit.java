6ab2216e7962750a16cef66c14c0c62dacbdf5f4
Baek Dongjae <kgdj030@gmail.com>
2023-11-29 23:22:50 +0900

    Title: 십의 자리 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231129/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+
+        for (int i = 0; i < 100; i += 1) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+            if (n >= 10) {
+                arr[n / 10 - 1] += 1;
+            }
+        }
+
+        for (int i = 0; i < 10; i += 1) {
+            System.out.printf("%d - %d\n", i + 1, arr[i]);
+        }
+    }
+}
