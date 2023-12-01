7c86cec9e8105d2494c70676649797583d055e63
Baek Dongjae <kgdj030@gmail.com>
2023-12-02 02:59:20 +0900

    Title: 대각선으로 숫자 채우기, Time: 530ms, Memory: 21MB, Status: Passed - CodeTree

--- 231202/대각선으로 숫자 채우기/diagonal-numbering.java
+++ 231202/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -7,15 +7,28 @@ public class Main {
         int n = sc.nextInt(), m = sc.nextInt(), cnt = 1;
         int[][] arr = new int[n][m];
 
-        for (int i = 0; i < n + m; i += 1 ) {
-            for (int j = 0; j < n; j += 1) {
-                for (int k = 0; k < m; k += 1) {
-                    if (i == j + k) {
-                        arr[j][k] = cnt;
-                        cnt += 1;
+        for (int startCol = 0; startCol < m; startCol += 1) {
+            int curRow = 0;
+            int curCol = startCol;
 
-                    }
-                }
+            while (curRow < n && curCol >= 0) {
+                arr[curRow][curCol] = cnt;
+                curRow += 1;
+                curCol -= 1;
+                cnt += 1;
+            }
+        }
+
+        for (int startRow = 1; startRow < n; startRow += 1) {
+            int curCol = m - 1;
+            int curRow = startRow;
+
+            while (curRow < n && curCol >= 0) {
+                arr[curRow][curCol] = cnt;
+
+                curRow += 1;
+                curCol -= 1;
+                cnt += 1;
             }
         }
 
