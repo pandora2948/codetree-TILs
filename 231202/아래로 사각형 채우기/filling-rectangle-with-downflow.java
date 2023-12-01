a6a060e5eb8ae1a50e8e21efe0aa449851d4f43b
Baek Dongjae <kgdj030@gmail.com>
2023-12-02 01:34:14 +0900

    Title: 아래로 사각형 채우기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231202/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), cnt = 1;
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                arr[j][i] = cnt;
+                cnt += 1;
+            }
+        }
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
