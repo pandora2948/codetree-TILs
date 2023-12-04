418c6bee3264b57a569b9b4417f11cdc29d49514
Baek Dongjae <kgdj030@gmail.com>
2023-12-04 23:24:34 +0900

    Title: 문자열 두번 출력하기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231204/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for (int i = 0; i < 2; i += 1) {
+            System.out.println(str);
+        }
+    }
+}
