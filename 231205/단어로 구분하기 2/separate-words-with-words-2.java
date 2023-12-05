cf9c7a2dbcb83373ed29f2d4f4b410755ff186d4
Baek Dongjae <kgdj030@gmail.com>
2023-12-05 17:14:23 +0900

    Title: 단어로 구분하기 2, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231205/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] strs = new String[10];
+
+        for (int i = 0; i < 10; i += 1) {
+            strs[i] = sc.next();
+            if (i % 2 == 0) {
+                System.out.println(strs[i]);
+            }
+        }
+    }
+}
