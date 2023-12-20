f950839a1f9d612a6316adc66133eeed520b4582
Baek Dongjae <kgdj030@gmail.com>
2023-12-20 21:01:14 +0900

    Title: 아스키코드표 맞추기, Time: 234ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231220/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i += 1) {
+            char c = (char) sc.nextInt();
+            System.out.printf("%c ", c);
+        }
+    }
+}
