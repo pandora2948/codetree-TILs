bdd8243c643f2a02eda97a663a1fb10de70174ae
Baek Dongjae <kgdj030@gmail.com>
2023-12-21 19:58:12 +0900

    Title: 숫자로 이루어진 사각형, Time: 484ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 231221/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    static int cnt = 1;
+    public static void printRect(int n) {
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                System.out.printf("%d ", cnt);
+                cnt = cnt + 1 > 9 ? 1 : cnt + 1;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printRect(n);
+    }
+}
