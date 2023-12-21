98a17e89241017b31fa56f35a1034d8ee23a054c
Baek Dongjae <kgdj030@gmail.com>
2023-12-21 19:07:42 +0900

    Title: 문자열의 개수, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 231221/문자열의 개수/number-of-spring.java
+++ 231221/문자열의 개수/number-of-spring.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int cnt = 0;
+        int cnt = 0, oddCnt = 0;
         String[] strings = new String[100];
 
         while (true) {
@@ -16,14 +16,16 @@ public class Main {
 
             
             if (cnt % 2 == 0) {
-                strings[cnt / 2] = str;
+                strings[oddCnt] = str;
+                oddCnt += 1;
             }
             cnt += 1;
             
         }
 
         System.out.println(cnt);
-        for (int i = 0; i <= cnt / 2; i += 1) {
+
+        for (int i = 0; i < oddCnt; i += 1) {
             System.out.println(strings[i]);
         }
     }
