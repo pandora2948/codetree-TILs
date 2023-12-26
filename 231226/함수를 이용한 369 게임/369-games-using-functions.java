25cda3b2b4a5f914e2818958701e492fc728054a
Baek Dongjae <kgdj030@gmail.com>
2023-12-26 23:35:06 +0900

    Title: 함수를 이용한 369 게임, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231226/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean checkNum(int n) {
+        return (n / 10) % 3 == 0 || n % 10 % 3 == 0;
+    }
+    
+    public static boolean multOfThree(int n) {
+        return n % 3 == 0;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = a; i <= b; i += 1) {
+            if (checkNum(i) || multOfThree(i)) {
+                cnt += 1;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
