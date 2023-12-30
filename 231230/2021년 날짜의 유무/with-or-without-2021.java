f3326992f24c24c04ba82b366d24e61e732c2465
Baek Dongjae <kgdj030@gmail.com>
2023-12-30 19:52:37 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231230/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt() - 1;
+        int d = sc.nextInt() - 1;
+        int[][] cal = new int[12][31];
+
+        for (int i = 0; i < 12; i += 1) {
+            if (i == 1) {
+                for (int j = 0; j < 28; j += 1) {
+                    cal[i][j] = 1;
+                }
+            }
+
+            if (i % 2 == 0) {
+                for (int j = 0; j < 31; j += 1) {
+                    cal[i][j] = 1;
+                }
+            }
+            else {
+                for (int j = 0; j < 30; j += 1) {
+                    cal[i][j] = 1;
+                }
+
+            }
+        }
+
+        System.out.println(cal[m][d] == 1 ? "Yes" : "No");
+        
+    }
+}
