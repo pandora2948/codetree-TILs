e9cca62ebe1e151a252f7224c01f8e5e5367d7df
Baek Dongjae <kgdj030@gmail.com>
2024-01-04 06:11:09 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240104/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+                
+public class Main {
+    public static int f(int n) {
+        if (n == 1) return 0;
+
+        return n % 2 == 0 ? f(n / 2) + 1 : f(n * 3 + 1) + 1;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(f(n));
+    }
+}
