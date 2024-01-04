870277e092c1d33bcb63d874298d53cc3fbc8f51
Baek Dongjae <kgdj030@gmail.com>
2024-01-04 22:08:24 +0900

    Title: 문자에 따른 명령 2, Time: 179ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240104/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char[] q = sc.next().toCharArray();
+        int d = 0;
+        int x = 0, y = 0;
+
+        for (int i = 0; i < q.length; i += 1) {
+            char query = q[i];
+
+            if (query == 'L') {
+                if (d - 1 < 0) {
+                    d = 3;
+                }
+                else {
+                    d = d - 1;
+                }
+                continue;
+            }
+
+            if (query == 'R') {
+                d += 1 % 4;
+                continue;
+            }
+            
+            switch (d) {
+                case 0:
+                    y += 1;
+                    break;
+
+                case 1:
+                    x += 1;
+                    break;
+
+                case 2:
+                    y -= 1;
+                    break;
+
+                case 3:
+                    x -= 1;
+                    break;
+            }
+
+        }
+        System.out.printf("%d %d", x, y);
+    }
+}
