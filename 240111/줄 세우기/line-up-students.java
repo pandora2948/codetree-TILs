4d49be91684ac8c3315a6d4dc10ea8fcf1f8e86f
Baek Dongjae <kgdj030@gmail.com>
2024-01-11 00:00:43 +0900

    Title: 줄 세우기, Time: 474ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 240111/줄 세우기/line-up-students.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Student implements Comparable<Student> {
+    int h;
+    int w;
+    int n;
+
+    Student(int h, int w, int n) {
+        this.h = h;
+        this.w = w;
+        this.n = n;
+    }
+
+    public void info() {
+        System.out.printf("%d %d %d\n", this.h, this.w, this.n);
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        if (this.h != s.h) {
+            return s.h - this.h;
+        }
+
+        if (this.w != s.w) {
+            return s.w - this.w;
+        }
+
+        return this.n - s.n;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] stds = new Student[n];
+
+        for (int i = 0; i < n; i += 1) {
+            stds[i] = new Student(sc.nextInt(), sc.nextInt(), i + 1);
+        }
+
+        Arrays.sort(stds);
+
+        for (int i = 0; i < n; i += 1) {
+            stds[i].info();
+        }
+    }
+}
