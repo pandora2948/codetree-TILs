0074edd6ed5c6403dbd1efa20a82f26bf2615a08
Baek Dongjae <kgdj030@gmail.com>
2024-01-20 14:21:46 +0900

    Title: 왔다 갔던 구역 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240120/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int pos = 1000;
+        int[] arr = new int[20001];
+        int res = 0;
+        int min = 2000;
+        int max = 0;
+
+        for (int i = 0; i < n; i += 1) {
+            int amount = sc.nextInt();
+            char direction = sc.next().charAt(0);
+
+            if (direction == 'R') {
+                for (int j = pos; j < pos + amount; j += 1) {
+                    arr[j] += 1;
+                }
+                pos += amount;
+                max = Math.max(max, pos);
+            }
+            else {
+                for (int j = pos; j > pos - amount; j -= 1) {
+                    arr[j] += 1;
+                }
+                pos -= amount;
+                min = Math.min(min, pos);
+            }
+        }
+
+        for (int i = min; i <= max; i += 1) {
+            if (arr[i] >= 2) {
+                res += 1;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
