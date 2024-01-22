d191acb047110e5fb1cbdf51890a39795f249b17
Baek Dongjae <kgdj030@gmail.com>
2024-01-22 19:01:29 +0900

    Title: 흰검 칠하기, Time: 216ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240122/흰검 칠하기/painting-white-black.java
+++ 240122/흰검 칠하기/painting-white-black.java
@@ -1,16 +1,25 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static int[] arr = new int[200001];
-
+// 위치의 타일을 색칠하는 함수
     public static void paintTile(int pos, int dir) {
         if (arr[pos] == 0) {
             arr[pos] = dir;
             return;
         }
 
+        if (arr[pos] == 4) {
+            return;
+        }
+
+        //  해당 타일의 색과 색칠 횟수를 각 -1, 2, -3 과 같이 표현하기 위한 로직
         arr[pos] = (Math.abs(arr[pos]) + 1) * (arr[pos] / Math.abs(arr[pos]) * (-1));
+        
+        if (Math.abs(arr[pos]) == 4) {
+            arr[pos] = 4;
+            return;
+        }
     }
 
     public static void main(String[] args) {
@@ -26,27 +35,27 @@ public class Main {
             int a = sc.nextInt();
             int d = sc.next().charAt(0) == 'L' ? -1 : 1;
 
-            while (a > 0) {
+            for (int j = 1; j <= a; j += 1) {
                 paintTile(pos, d);
-                a -= 1;
-                if (a > 0) pos += d;
+                if (a > j) pos += d;
             }
-
+            // 배열에서 탐색을 적게 하기위한 탐색 범위 지정 변수 min, max
             min = Math.min(min, pos);
             max = Math.max(max, pos);
         }
 
         for (int i = min; i <= max; i += 1) {
-            if (Math.abs(arr[i]) >= 4) {
+            // 각 2번이상 색칠되어져 회색 : 요소의 절대값이 4 이상
+            if (arr[i] == 4) {
                 res[2] += 1;
                 continue;
             }
-
+            // 요소가 음수일 경우 흰색으로 색칠되어있음
             if (arr[i] < 0) {
                 res[0] += 1;
                 continue;
             }
-
+            // 4 이하의 양수일 경우 검은색
             res[1] += 1;
         }
 
