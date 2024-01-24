3a3343a37c97865a8733b45572c12fdb12a3d3a6
Baek Dongjae <kgdj030@gmail.com>
2024-01-24 18:54:22 +0900

    Title: 연속되는 수 3, Time: 187ms, Memory: 12MB, Status: Passed - CodeTree

--- 240124/연속되는 수 3/continuous-number3.java
+++ 240124/연속되는 수 3/continuous-number3.java
@@ -15,7 +15,7 @@ public class Main {
         item[0] = arr[0] / Math.abs(arr[0]);
         item[1] = 1;
 
-        for (int i = 0; i < n ; i += 1) {
+        for (int i = 1; i < n ; i += 1) {
             if (arr[i] / Math.abs(arr[i]) != item[0]) {
                 item[0] = arr[i] / Math.abs(arr[i]);
                 item[1] = 1;
