d8730af327cb69521c1afa17e637b5ba5a76cf91
Baek Dongjae <kgdj030@gmail.com>
2024-01-28 18:55:58 +0900

    Title: 좌우로 움직이는 로봇, Time: 934ms, Memory: 58MB, Status: Passed - CodeTree

--- 240128/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240128/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -9,16 +9,16 @@ public class Main {
         int[] arrA = new int[1000001];
         int[] arrB = new int[1000001];
         int cnt = 0;
-        int timeA = 1;
-        int timeB = 1;
+        int timeA = 0;
+        int timeB = 0;
 
         for (int i = 0; i < n; i += 1) {
             int t = sc.nextInt();
             char d = sc.next().charAt(0);
 
             for (int j = 0; j < t; j += 1) {
-                arrA[timeA] = d == 'L' ? arrA[timeA - 1] - 1 : arrA[timeA - 1] + 1;
                 timeA += 1;
+                arrA[timeA] = d == 'L' ? arrA[timeA - 1] - 1 : arrA[timeA - 1] + 1;
             }
         }
 
@@ -27,24 +27,27 @@ public class Main {
             char d = sc.next().charAt(0);
 
             for (int j = 0; j < t; j += 1) {
-                arrB[timeB] = d == 'L' ? arrA[timeB - 1] - 1 : arrA[timeB - 1] + 1;
+                timeB += 1;
+                arrB[timeB] = d == 'L' ? arrB[timeB - 1] - 1 : arrB[timeB - 1] + 1;
             }
         }
 
+        int minLen = Math.min(timeA, timeB);
         int maxLen = Math.max(timeA, timeB);
 
-        if (timeA > timeB) {
-            for(int i = timeB; i <= timeA; i += 1) {
-                arrB[i] = arrB[timeB];
+        for (int i = minLen; i <= maxLen; i += 1) {
+            if (timeA > timeB) {
+                arrB[i] = arrB[minLen];
+                continue;
             }
-        }
-        else if (timeA < timeB) {
-            for (int i = timeA; i <= timeB; i += 1) {
-                arrA[i] = arrA[timeA];
+
+            if (timeA < timeB) {
+                arrA[i] = arrA[minLen];
+                continue;
             }
         }
 
-        for (int i = 2; i <= maxLen; i += 1) {
+        for (int i = 1; i <= maxLen; i += 1) {
             if (arrA[i - 1] != arrB[i - 1] && arrA[i] == arrB[i]) {
                 cnt += 1;
             }
