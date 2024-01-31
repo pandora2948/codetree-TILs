fdf12a7df978e2a3f3a079a0f3030fd1a8473759
Baek Dongjae <kgdj030@gmail.com>
2024-01-31 23:44:34 +0900

    Title: 모이자, Time: 249ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240131/모이자/gather.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i += 1) {
+            arr[i] = sc.nextInt();
+        }
+
+        int idx = 0;
+        for (int i = 0; i < n; i += 1) {
+            int d = 0;
+            for (int j = 0; j < n; j += 1) {
+                d += Math.abs(j - idx) * arr[j];
+            }
+            min = Math.min(min, d);
+            idx += 1;
+        }
+        System.out.println(min);
+    }
+}
