8473a734dd9d8977333128e408bb35211f3ac0f3
Baek Dongjae <kgdj030@gmail.com>
2024-01-31 18:07:26 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 526ms, Memory: 21MB, Status: Passed - CodeTree

--- 240131/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 240131/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class Main {
-    public static boolean isInside(int x, int y, int n) {
-        return 0 <= x && x < n && 0 <= y && y < n;
+    public static boolean isInside(int x, int y, int n, int m) {
+        return 0 <= x && x < m && 0 <= y && y < n;
     }
 
     public static void main(String[] args) {
@@ -10,27 +10,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
         int[][] tbl = new int[n][m];
-
+        int x = 0, y = 0;
+        int d = 0;
         int[] dx = new int[] {0, 1, 0, -1};
         int[] dy = new int[] {1, 0, -1, 0};
-        int d = 0;
-        int x = 0, y = 0;
         int cnt = 1;
 
         while (cnt <= n * m) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
             tbl[y][x] = cnt;
             cnt += 1;
-            
-            if (!isInside(nx, ny, n) || tbl[ny][nx] != 0) {
+
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (!isInside(nx, ny, n, m) || tbl[ny][nx] != 0) {
                 d = (d + 1) % 4;
-                nx = x + dx[d];
-                ny = y + dy[d];
             }
 
-            x = nx;
-            y = ny;
+            x += dx[d];
+            y += dy[d];
         }
 
         for (int i = 0; i < n; i += 1) {
