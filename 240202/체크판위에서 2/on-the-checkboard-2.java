c1b8e97572753622089f70ed6a37d70c5f91b1af
Baek Dongjae <kgdj030@gmail.com>
2024-02-02 15:02:15 +0900

    Title: 체크판위에서 2, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240202/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int cnt = 0;
+        char[][] tbl = new char[r][c];
+        for (int i = 0; i < r; i += 1) {
+            for (int j = 0; j < c; j += 1) {
+                tbl[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        for (int i = 1; i < r - 1; i += 1) {
+            for (int j = 1; j < c - 1; j += 1) {
+                for (int k = i + 1; k < r - 1; k += 1) {
+                    for (int l = j + 1; l < c - 1; l += 1) {
+                        if (tbl[i][j] == 'B' && tbl[k][l] == 'W') {
+                            cnt += 1;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
