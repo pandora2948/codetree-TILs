5fbc927eba60a3a6a20e0573568f9b25003831f7
Baek Dongjae <kgdj030@gmail.com>
2024-02-02 16:00:55 +0900

    Title: 이상한 진수 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/이상한 진수 2/awkward-digits-2.java
+++ 240202/이상한 진수 2/awkward-digits-2.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
+        char[] a = sc.next().toCharArray();
         int max = 0;
 
-        for (int i = 0; i < a.length(); i += 1) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(a.substring(0, i)).append(1).append(a.substring(i + 1, a.length()));
-            max = Math.max(max, Integer.parseInt(sb.toString(), 2));
+        for (int i = 0; i < a.length; i += 1) {
+            char[] aPrime = Arrays.copyOf(a, a.length);
+            aPrime[i] = aPrime[i] == '0' ? '1' : '0';
+            max = Math.max(max, Integer.parseInt(String.copyValueOf(aPrime), 2));
         }
 
-        System.out.println(max == Integer.parseInt(a, 2) ? 0 : max);
+        System.out.println(max);
     }
 }
