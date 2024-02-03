da576686aa72311ca7fe1cbdf9b95e2d54f52ad6
Baek Dongjae <kgdj030@gmail.com>
2024-02-03 16:56:46 +0900

    Title: 오목, Time: 157ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240203/오목/O-mok.java
@@ -0,0 +1,126 @@
+import java.util.*;
+
+public class Main {
+    public static void printWin(int c, int d, int i, int j) {
+        System.out.println(c);
+        int x = 0;
+        int y = 0;
+
+        switch (d) {
+            case 1:
+                x = j + 3;
+                y = i + 1;
+                break;
+
+            case 2:
+                x = j + 1;
+                y = i + 3;
+                break;
+
+            case 3:
+                x = j + 3;
+                y = i + 3;
+                break;
+            
+            case 4:
+                x = j + 3;
+                y = i - 1;
+        }
+        
+        System.out.printf("%d %d", y, x);
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        final int n = 19;
+        int[][] tbl = new int[n][n];
+        int winColor = 0;
+        int winDirection = 0;
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                tbl[i][j] = sc.nextInt();
+            }
+        }
+        
+        // 가로
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n - 5; j += 1) {
+                int c = tbl[i][j];
+                if (c == 0) continue;
+
+                boolean isWin = true;
+                for (int l = 0; l < 5; l += 1) {
+                    if (tbl[i][j + l] != c) {
+                        isWin = false;
+                    }
+                }
+                if (isWin) {
+                    printWin(c, 1, i, j);
+                    return;
+                }
+            }
+        }
+
+
+        // 세로
+        for (int i = 0; i < n - 5; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                int c = tbl[i][j];
+                if (c == 0) continue;
+
+                boolean isWin = true;
+                for (int k = 0; k < 5; k += 1) {
+                    if (tbl[i + k][j] != c) {
+                        isWin = false;
+                    }
+                }
+                if (isWin) {
+                    printWin(c, 2, i, j);
+                    return;
+                }
+            }
+        }
+
+        // 대각(하)
+        for (int i = 0; i < n - 5; i += 1) {
+            for (int j = 0; j < n - 5; j += 1) {
+                int c = tbl[i][j];
+                if (c == 0) continue;
+
+                boolean isWin = true;
+                for (int k = 0; k < 5; k += 1) {
+                    if (tbl[i + k][j + k] != c) {
+                        isWin = false;
+                    }
+                }
+                
+                if (isWin) {
+                    printWin(c, 3, i, j);
+                    return;
+                }
+            }
+        }
+
+        for (int i = 4; i < n; i += 1) {
+            for (int j = 0; j < n - 5; j += 1) {
+                int c = tbl[i][j];
+                if (c == 0) continue;
+
+                boolean isWin = true;
+                for (int k = 0; k < 5; k += 1) {
+                    if (tbl[i - k][j + k] != c) {
+                        isWin = false;
+                    }
+                }
+                if (isWin) {
+                    printWin(c, 4, i , j);
+                    return;
+                }
+            }
+        }
+
+
+    }
+}
