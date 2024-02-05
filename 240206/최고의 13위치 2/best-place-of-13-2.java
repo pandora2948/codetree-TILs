c69778bdf3f5339e4c48c1fb08b1eb3952fcd947
Baek Dongjae <kgdj030@gmail.com>
2024-02-06 03:28:08 +0900

    Title: 최고의 13위치 2, Time: 176ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/최고의 13위치 2/best-place-of-13-2.java
+++ 240206/최고의 13위치 2/best-place-of-13-2.java
@@ -1,15 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static boolean isInside(int x, int n) {
-        return 0 <= x && x < n;
-    }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int max = 0;
         int[][] tbl = new int[n][n];
-        int[] res = new int[2];
 
         for (int i = 0; i < n; i += 1) {
             for (int j = 0; j < n; j += 1) {
@@ -18,52 +15,20 @@ public class Main {
         }
 
         for (int i = 0; i < n; i += 1) {
-            for (int j = 0; j < n; j += 1) {
-                if (!isInside(j + 2, n)) continue;
-
-                int cnt = 0;
-                for (int k = 0; k < 3; k += 1) {
-                    if (tbl[i][j + k] == 1) {
-                        cnt += 1;
-                    }
-                }
-                res[0] = Math.max(res[0], cnt);
-            }
-        }
-
-        for (int i = 0; i < n; i += 1) {
-            for (int j = 0; j < n; j += 1) {
-                if (!isInside(j + 2, n)) continue;
-
-                int cnt = 0;
-                for (int k = 0; k < 3; k += 1) {
-                    if (tbl[i][j + k] == 1) cnt += 1;
-                }
-                
-                if (res[0] == cnt) {
-                    for (int k = 0; k < 3; k += 1) {
-                        if (tbl[i][j + k] == 1) {
-                            tbl[i][j + k] = 0;
+            for (int j = 0; j < n - 2; j += 1) {
+                for (int k = 0; k < n; k += 1) {
+                    for (int l = 0; l < n - 2; l += 1) {
+                        if (i == k && Math.abs(j - l) <= 2) continue;
+                        int sum = 0;
+                        for (int m = 0; m < 3; m += 1) {
+                            sum += tbl[i][j + m] + tbl[k][l + m];
                         }
+                        max = Math.max(max, sum);
                     }
                 }
             }
         }
+        System.out.println(max);
 
-        for (int i = 0; i < n; i += 1) {
-            for (int j = 0; j < n; j += 1) {
-                if (!isInside(j + 2, n)) continue;
-
-                int cnt = 0;
-                for (int k = 0; k < 3; k += 1) {
-                    if (tbl[i][j + k] == 1) {
-                        cnt += 1;
-                    }
-                }
-                res[1] = Math.max(res[1], cnt);
-            }
-        }
-
-        System.out.println(res[0] + res[1]);
     }
 }
