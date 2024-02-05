83ac14980c5c1cac89f84066eb78693f0757fd5b
Baek Dongjae <kgdj030@gmail.com>
2024-02-06 02:04:05 +0900

    Title: 최고의 13위치 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240206/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isInside(int x, int n) {
+        return 0 <= x && x < n;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] tbl = new int[n][n];
+        int[] res = new int[2];
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                tbl[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                if (!isInside(j + 2, n)) continue;
+
+                int cnt = 0;
+                for (int k = 0; k < 3; k += 1) {
+                    if (tbl[i][j + k] == 1) cnt += 1;
+                }
+                
+                if (res[0] < cnt) {
+                    res[0] = cnt;
+
+                    for (int k = 0; k < 3; k += 1) {
+                        if (tbl[i][j + k] == 1) {
+                            tbl[i][j + k] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                if (!isInside(j + 2, n)) continue;
+
+                int cnt = 0;
+                for (int k = 0; k < 3; k += 1) {
+                    if (tbl[i][j + k] == 1) {
+                        cnt += 1;
+                    }
+                }
+                res[1] = Math.max(res[1], cnt);
+            }
+        }
+
+        System.out.println(res[0] + res[1]);
+    }
+}
