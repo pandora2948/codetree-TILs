228d14b95b8a0561adc081715c7bc77a41d249fb
Baek Dongjae <kgdj030@gmail.com>
2024-02-06 03:34:29 +0900

    Title: 특정 수와 근접한 합, Time: 170ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240206/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int[] arr = new int[n];
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i += 1) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                if (i == j) continue;
+
+                int sum = 0;
+                for (int k = 0; k < n; k += 1) {
+                    if (k == i || k == j) continue;
+
+                    sum += arr[k];
+                }
+
+                min = Math.min(min, Math.abs(sum - s));
+            }
+        }
+
+        System.out.println(min);
+
+
+    }
+}
