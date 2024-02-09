c1786780bc02bb810e5ab5b938c51052e3776f59
Baek Dongjae <kgdj030@gmail.com>
2024-02-09 19:14:19 +0900

    Title: G or H 2, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240209/G or H 2/G-or-H-2.java
+++ 240209/G or H 2/G-or-H-2.java
@@ -21,12 +21,12 @@ public class Main {
         }
 
         for (int i = 0; i < MAX_POS; i += 1) {
-            for (int j = i + 1; j < MAX_POS; j += 1) {
-                if (arr[i] == 'X' || arr[j - 1] == 'X') continue;
+            for (int j = i; j < MAX_POS; j += 1) {
+                if (arr[i] == 'X' || arr[j] == 'X') continue;
 
                 int gCount = 0;
                 int hCount = 0;
-                for (int k = i; k < j; k += 1) {
+                for (int k = i; k <= j; k += 1) {
                     if (arr[k] == 'G') {
                         gCount += 1;
                         continue;
@@ -41,7 +41,7 @@ public class Main {
                 if (gCount + hCount == 0) continue;
 
                 if (gCount == hCount) {
-                    res = Math.max(res, j - i - 1);
+                    res = Math.max(res, j - i);
                 }
 
             }
