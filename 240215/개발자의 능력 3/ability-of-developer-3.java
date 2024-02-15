c264d362680441069196049724bf3a36d151acfb
Baek Dongjae <kgdj030@gmail.com>
2024-02-15 15:45:14 +0900

    Title: 개발자의 능력 3, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240215/개발자의 능력 3/ability-of-developer-3.java
+++ 240215/개발자의 능력 3/ability-of-developer-3.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Main {
     public static int n = 6;
     public static int[] arr = new int[n];
-    public static int getDiff(int a, int b) {
-        int s1 = arr[a] + arr[b];
+    public static int getDiff(int a, int b, int c) {
+        int s1 = arr[a] + arr[b] + arr[c];
         int s2 = 0;
         for (int i = 0; i < n; i += 1) {
             s2 += arr[i];
         }
         s2 -= s1;
-        
+
         return Math.abs(s1 - s2);
     }
     public static void main(String[] args) {
@@ -24,7 +24,9 @@ public class Main {
 
         for (int i = 0; i < n; i += 1) {
             for (int j = i + 1; j < n; j += 1) {
-                res = Math.min(res, getDiff(i, j));
+                for (int k = j + 1; k < n; k += 1) {
+                    res = Math.min(res, getDiff(i, j, k));
+                }
             }
         }
         System.out.println(res);
