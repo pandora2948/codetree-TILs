febdd3e68e22131c775b79a495e2265952bd3ffe
Baek Dongjae <kgdj030@gmail.com>
2024-03-22 14:24:33 +0900

    Title: 조합, Time: 3035ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 240322/조합/combination.java
+++ 240322/조합/combination.java
@@ -1,31 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int n;
-    public static int m;
-    public static int[] arr;
-    public static int cnt = 0;
-
-    public static void combination(int idx, int level) {
-        if (level == m) {
-            cnt += 1;
-            return;
+    public static int getCombi(int n, int r) {
+        if (n == r || r == 0) {
+            return 1;
         }
-
-        for (int i = idx; i < n; i += 1) {
-            combination(i + 1, level + 1);
-        }
+        return getCombi(n - 1, r - 1) + getCombi(n - 1, r);
     }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        
-        n = sc.nextInt();
-        m = sc.nextInt();
-        arr = new int[n];
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-        combination(0, 0);
-
-        System.out.println(cnt);
+        System.out.println(getCombi(n, m));
     }
 }
