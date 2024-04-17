c0c805dcef1b0d242002858553a03abbc4471313
Baek Dongjae <kgdj030@gmail.com>
2024-04-17 19:02:48 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240417/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
+++ 240417/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int calcLen(int x1, int x2, int y1, int y2) {
-        return Math.abs(x2 - x1) + Math.abs(y2 - y1);
-    }
-
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] arr = new int[n][2];
-        int[] min = new int[3];
+        int[] min = new int[2];
 
-        for (int i = 0; i < 3; i += 1) {
+        for (int i = 0; i < 2; i += 1) {
             min[i] = Integer.MAX_VALUE;
         }
 
@@ -24,11 +20,16 @@ public class Main {
         for (int i = 0; i < n; i += 1) {
             for (int j = 0; j < n; j += 1) {
                 if (i == j) continue;
+                int x1 = arr[i][0];
+                int x2 = arr[j][0];
+                int y1 = arr[i][0];
+                int y2 = arr[j][0];
+                int w = Math.abs(x1 - x2);
+                int h = Math.abs(y1 - y2);
 
-                if (calcLen(arr[i][0], arr[j][0], arr[i][1], arr[j][1]) < min[2]) {
-                    min[0] = Math.abs(arr[i][0] - arr[j][0]);
-                    min[1] = Math.abs(arr[i][1] - arr[j][1]);
-                    min[2] = calcLen(arr[i][0], arr[j][0], arr[i][1], arr[j][1]);
+                if (w + h < min[0] + min[1]) {
+                    min[0] = w;
+                    min[1] = h;
                 }
             }
         }
