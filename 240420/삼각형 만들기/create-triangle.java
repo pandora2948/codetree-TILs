a64c5fa8c6286bf4640872bb346e89a00fa8b267
Baek Dongjae <kgdj030@gmail.com>
2024-04-20 17:05:08 +0900

    Title: 삼각형 만들기, Time: 185ms, Memory: 11MB, Status: Passed - CodeTree

--- 240420/삼각형 만들기/create-triangle.java
+++ 240420/삼각형 만들기/create-triangle.java
@@ -17,37 +17,37 @@ public class Main {
         for (int i = 0; i < n; i += 1) {
             for (int j = 0; j < n; j += 1) {
                 for (int k = 0; k < n; k += 1) {
-                    if (i == j || j == k || k == j) continue;
+                    if (i == j || j == k || k == i) continue;
                     int x1 = arr[i][0], x2 = arr[j][0], x3 = arr[k][0];
                     int y1 = arr[i][1], y2 = arr[j][1], y3 = arr[k][1];
 
                     if (x1 != x2 && y1 == y2) {
                         if (x1 == x3 && y1 != y3) {
-                            res = Math.abs(x2 - x1) * Math.abs(y3 - y1);
+                            res = Math.max(res, Math.abs(x2 - x1) * Math.abs(y3 - y1));
                         }
 
                         else if (x2 == x3 && y2 != y3) {
-                            res = Math.abs(x3 - x2) * Math.abs(y3 - y2);
+                            res = Math.max(res, Math.abs(x2 - x1) * Math.abs(y3 - y2));
                         }
                     }
 
                     else if (x2 != x3 && y2 == y3) {
                         if (x1 == x2 && y1 != y2) {
-                            res = Math.abs(x3 - x2) * Math.abs(y2 - y1);
+                            res = Math.max(res, Math.abs(x3 - x2) * Math.abs(y2 - y1));
                         }
 
                         else if (x1 == x3 && y1 != y3) {
-                            res = Math.abs(x3 - x2) * Math.abs(y3 - y1);
+                            res = Math.max(res, Math.abs(x3 - x2) * Math.abs(y3 - y1));
                         }
                     }
 
                     else if (x1 != x3 && y1 == y3) {
                         if (x1 == x2 && y1 != y2) {
-                            res = Math.abs(x3 - x1) * Math.abs(y2 - y1);
+                            res = Math.max(res, Math.abs(x3 - x1) * Math.abs(y2 - y1));
                         }
 
                         else if (x2 == x3 && y2 != y3) {
-                            res = Math.abs(x3 - x1) * Math.abs(y3 - y2);
+                            res = Math.max(res, Math.abs(x3 - x1) * Math.abs(y3 - y2));
                         }
                     }
 
