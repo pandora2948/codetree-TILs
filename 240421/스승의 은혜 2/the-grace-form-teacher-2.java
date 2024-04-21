d859a94943d81caa96d9a1186ef3f5416f64c221
Baek Dongjae <kgdj030@gmail.com>
2024-04-21 18:31:50 +0900

    Title: 스승의 은혜 2, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240421/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int cost = 0;
+        int res = 0;
+
+        Integer[] arr = new Integer[n];
+        
+        for (int i = 0; i < n; i += 1) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i += 1) {
+            if (cost + arr[i] > b) {
+                if (cost + (arr[i] / 2) <= b) {
+                    res = i + 1;
+                }
+                else {
+                    res = i;
+                }
+            }
+
+            cost += arr[i];
+        }
+
+        System.out.println(res);
+    }
+}
