4517f86a2c6d67e5e4f96a638a21678ee3e7dded
Baek Dongjae <kgdj030@gmail.com>
2024-05-08 14:03:04 +0900

    Title: 아름다운 수열, Time: 221ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240508/아름다운 수열/beautiful-sequence.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        int[] arrA = new int[n];
+
+        for (int i = 0; i < n; i += 1) {
+            arrA[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] arrB = new int[m];
+        int[] res = new int[n - m];
+
+        for (int i = 0; i < m ; i += 1) {
+            arrB[i] = sc.nextInt();
+        }
+        Arrays.sort(arrB);
+
+        for (int i = 0; i < n - m + 1; i += 1) {
+            int[] tarArray = Arrays.copyOfRange(arrA, i, i + m);
+            Arrays.sort(tarArray);
+            int diff = Math.abs(tarArray[0] - arrB[0]);
+            boolean isBeauty = true;
+
+            for (int j = 0; j < m; j += 1) {
+                if (Math.abs(tarArray[j] - arrB[j]) != diff) {
+                    isBeauty = false;
+                    break;
+                }
+            }
+
+            if (isBeauty) {
+                res[cnt] = i + 1;
+                cnt += 1;
+            }
+        }
+
+        System.out.println(cnt);
+
+        for (int i = 0; i < cnt; i += 1) {
+            System.out.println(res[i]);
+        }
+
+    }
+}
