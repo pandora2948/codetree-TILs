1da384eef34d76769e05c7536414e3c9245fcf4c
Baek Dongjae <kgdj030@gmail.com>
2024-05-27 21:39:22 +0900

    Title: 정수 명령 처리, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240527/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10001];
+
+        int n = Integer.parseInt(br.readLine());
+        int idx = 0;
+        
+        for (int i = 0; i < n; i += 1) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch(st.nextToken()) {
+                case "push":
+                    arr[idx] = Integer.parseInt(st.nextToken());
+                    idx += 1;
+                    break;
+
+                case "pop":
+                    System.out.println(arr[idx]);
+                    arr[idx] = 0;
+                    idx -= 1;
+                    break;
+
+                case "size":
+                    System.out.println(idx + 1);
+                    break;
+
+                case "empty":
+                    System.out.println(idx == 0 ? "1" : "0");
+                    break;
+
+                case "top":
+                    System.out.println(arr[idx]);
+                    break;
+            }
+        }
+        
+        
+    }
+}
