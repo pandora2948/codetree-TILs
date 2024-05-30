4fc7ea5169b9155eca2c93d44c095cc95b75c9ab
Baek Dongjae <kgdj030@gmail.com>
2024-05-30 23:19:20 +0900

    Title: 격자 늘리기, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240530/격자 늘리기/stretch-grid.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        char[][] tbl = new char[n][m];
+
+        for (int i = 0; i < n; i += 1) {
+            tbl[i] = br.readLine().toCharArray();
+        }
+
+        for (int i = 0; i < n; i += 1) {
+            for (int p = 0; p < k; p += 1) {
+                for (int j = 0; j < m; j += 1) {
+                    for (int l = 0; l < k; l += 1) {
+                        bw.write(tbl[i][j]);
+                    }
+                }
+                bw.newLine();
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
