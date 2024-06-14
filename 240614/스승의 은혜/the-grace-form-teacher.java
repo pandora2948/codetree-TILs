5a1c57030d292aec6ac6cb0b481cf0bd367004b3
Baek Dongjae <kgdj030@gmail.com>
2024-06-14 19:57:49 +0900

    Title: 스승의 은혜, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240614/스승의 은혜/the-grace-form-teacher.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int totalCost = 0;
+        int cnt = 0;
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i += 1) {
+            st = new StringTokenizer(br.readLine());
+            
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        while (totalCost < b) {
+            totalCost += arr[cnt][0] + arr[cnt][1];
+            cnt += 1;
+        }
+
+        if (totalCost + (arr[cnt][0] / 2 + arr[cnt][1]) < b) {
+            cnt += 1;
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+
+    }
+}
