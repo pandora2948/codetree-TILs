d52151a8c41a0ad293406c261e3ede4418e48624
Baek Dongjae <kgdj030@gmail.com>
2024-06-20 18:55:57 +0900

    Title: 십진수와 이진수, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

--- 240620/십진수와 이진수/decimal-and-binary-number.java
+++ 240620/십진수와 이진수/decimal-and-binary-number.java
@@ -1,13 +1,14 @@
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String n = br.readLine();
+        BigInteger n = new BigInteger(br.readLine(), 2);
 
-        bw.write(Long.toBinaryString(Long.parseLong(n, 2) * 17));
+        bw.write(n.multiply(BigInteger.valueOf(17)).toString(2));
         bw.flush();
         bw.close();
     }
