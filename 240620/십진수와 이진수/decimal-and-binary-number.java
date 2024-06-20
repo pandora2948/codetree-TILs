db54c7a954f338046fee15d20ed3eda16a5675bb
Baek Dongjae <kgdj030@gmail.com>
2024-06-20 18:27:58 +0900

    Title: 십진수와 이진수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240620/십진수와 이진수/decimal-and-binary-number.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine(), 2);
+
+        bw.write(Integer.toBinaryString(n * 17));
+        bw.flush();
+        bw.close();
+    }
+}
