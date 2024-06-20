60321386f404becf23b3c617c2653c160d01f5c7
Baek Dongjae <kgdj030@gmail.com>
2024-06-20 21:10:17 +0900

    Title: 패턴 출력하는 재귀함수, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

--- 240620/패턴 출력하는 재귀함수/recursive-function-that-outputs-pattern.java
+++ 240620/패턴 출력하는 재귀함수/recursive-function-that-outputs-pattern.java
@@ -2,33 +2,28 @@ import java.io.*;
 
 public class Main {
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void compare(int num) throws IOException {
+        if (num == 369) {
+            bw.write(String.valueOf(num) + " ");
+        }
 
-    public static void printPattern(int num) throws IOException {
-        if (num < 369) return;
-
-        printPattern(num - 2);
-        bw.write(String.valueOf(num) + " ");
+        if (num < 369) {
+            bw.write(String.valueOf(num) + " ");
+            compare(num + 2);
+        }
+        
+        if (num > 369) {
+            compare(num - 2);
+            bw.write(String.valueOf(num) + " ");
+        }
     }
 
-    public static void printSecondPattern(int num, int bound) throws IOException {
-        if (bound > num) return;
-
-        printSecondPattern(num - 2, bound);
-        bw.write(String.valueOf(num) + " ");
-    }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        if (n > 369) {
-            if (n % 2 == 0) printPattern(n - 1);
-            else printPattern(n);
-        }
-        else {
-            printSecondPattern(369, n);
-        }
+        compare(n);
 
         bw.flush();
         bw.close();
