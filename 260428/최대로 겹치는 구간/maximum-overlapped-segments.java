b5f985e75693dcea700828c1503c897408c6f932
Baek Dongjae <kgdj030@gmail.com>
2026-04-28 00:37:16 +0900

    Title: 최대로 겹치는 구간, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260428/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] lines = new int[200 + 1];
+
+        for (int i = 0; i < n; i += 1) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken()),
+                b = Integer.parseInt(st.nextToken());
+
+            if (a < 0) {
+                a = 100 - a;
+            }
+            if (b < 0) {
+                b = 100 - b;
+            }
+
+            for (int j = a; j < b; j += 1) {
+                lines[j - 1] += 1;
+            }
+        }
+
+        int res = 0;
+
+        for (int i = 0; i < 200; i += 1) {
+            res = Math.max(res, lines[i]);
+        }
+
+        bw.write(String.valueOf(res));
+        bw.flush();
+        bw.close();
+    }
+}
