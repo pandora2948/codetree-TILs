c4891919c34517aeedfa9a9cd70f5969fa534b90
Baek Dongjae <kgdj030@gmail.com>
2026-05-09 09:12:40 +0900

    [Passed] 숫자 피라미드 (45ms, 16MB)

---
+++ trail0/숫자 피라미드/number-pyramid.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(str(i) + " ")
+    print("\n")
