1dce2c2107eff5d4c6f7a8908abaa43e69f6a536
Baek Dongjae <kgdj030@gmail.com>
2026-05-11 13:19:57 +0900

    [Passed] 동전이 있는 위치 (47ms, 16MB)

---
+++ trail1/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+tbl = [[0 for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    r, c = map(int, input().split())
+    tbl[r - 1][c - 1] = 1
+
+for items in tbl:
+    for el in items:
+        print(el, end=" ")
+    print()
