9522bcd1481a962771a123343ada030669ae6114
Baek Dongjae <kgdj030@gmail.com>
2026-05-11 13:22:19 +0900

    [Passed] 격자에 점 그리기 (50ms, 16MB)

---
+++ trail1/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+tbl = [[0 for _ in range(n)] for _ in range(n)]
+
+
+for i in range(m):
+    r, c = map(int, input().split())
+
+    tbl[r - 1][c - 1] = i + 1
+
+for items in tbl:
+    for el in items:
+        print(el, end=" ")
+    print()
